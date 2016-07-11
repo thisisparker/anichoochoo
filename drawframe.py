68e5db9520d00f56d1a821b63c9f52e54b55730a
Parker Higgins <parker@parkerhiggins.net>
2016-07-10 21:06:38 -0700

    v1.0 of desert scene animation

--- drawframe.py
+++ drawframe.py
@@ -3,81 +3,118 @@
 # More to come.
 
 from PIL import Image
-import random, json
-import glob, os
+import random
+from math import ceil
 
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 576
+SCREEN_SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
 NUMROWS = 6
 BLOCK_SIDE = int(SCREEN_HEIGHT/NUMROWS)
 BLOCK_SIZE = (BLOCK_SIDE,BLOCK_SIDE)
 ROWS = [BLOCK_SIDE * x for x in range(NUMROWS)]
+FRAME_COUNT = 160
 
 choochoo = Image.open("images/choochoo.png").resize(BLOCK_SIZE)
 redcar = Image.open("images/redcar.png").resize(BLOCK_SIZE)
 greencar = Image.open("images/greencar.png").resize(BLOCK_SIZE)
 cactus = Image.open("images/cactus.png").resize(BLOCK_SIZE)
+palm = Image.open("images/palm.png").resize(BLOCK_SIZE)
+turtle = Image.open("images/turtle.png").resize(BLOCK_SIZE)
+horse = Image.open("images/horse.png").resize(BLOCK_SIZE)
 sun = Image.open("images/sun.png").resize(BLOCK_SIZE)
+moon = Image.open("images/moon.png").resize(BLOCK_SIZE)
 blank = Image.new("RGBA",BLOCK_SIZE,(0,0,0,0))
 
 def main():
 
-    static = Image.new('RGBA',(SCREEN_WIDTH,SCREEN_HEIGHT),(255,255,255))
+    # "static" holds the elements that do not change throughout the animation.
+    # That includes the train and its cars.
 
-    static.paste(sun,(0,0),sun)
+    static = Image.new('RGBA',SCREEN_SIZE,(0,0,0,0))
 
-    # choochx = random.randint(0,SCREEN_WIDTH)
     choochx = int(BLOCK_SIDE * 6)
     choochy = ROWS[3]
-    placechoochoo(static,choochx,choochy)
+    place_engine(static,choochx,choochy)
 
-    # trainlength = random.randint(1,10)
-    trainlength = 7
+    train_length = 7
 
     carx = choochx
 
-    for car in range(trainlength):
+    for car in range(train_length):
         carx = carx + BLOCK_SIDE
-        placecar(static,carx,choochy)
+        place_car(static,carx,choochy)
 
-    background = Image.new('RGBA',(6*SCREEN_WIDTH,SCREEN_HEIGHT),(0,0,0,0))
+    # "sky_row" is a long strip that has one sun and moon, advancing slowly.
 
+    sky_row = Image.new('RGBA',(2 * SCREEN_WIDTH,BLOCK_SIDE),(0,0,0,0))
+    sky_row.paste(sun,(SCREEN_WIDTH,0),sun)
+    sky_row.paste(moon,(0,0),moon)
+
+    # Each item in the "minutes" list is a frame on which the sky_row advances.
+
+    minutes = [int(FRAME_COUNT/40 * _) for _ in range(1,40)] 
+
+    sky_offset = SCREEN_WIDTH
+
+    # Start with one frame full of scenery, and set the offset to zero. It'll create more along the way.
+
+    scenery = Image.new('RGBA',SCREEN_SIZE,(0,0,0,0))
     for row in [ROWS[1],ROWS[2],ROWS[4],ROWS[5]]:
-        placescenery(background,row)
+        place_scenery(scenery,row)
+    offset = 0
 
-    render = static.copy()
+    frame_number = 0
 
-    offset = background.width - render.width
+    while frame_number <= FRAME_COUNT:
 
-    img_suffix = 0
+        bgcolor = (255,255,255)
 
-    while offset >= 0:
-        render = static.copy()
-        render.paste(background,(-offset,0),background)
-        offset = offset - BLOCK_SIDE
-        new_filename = "slides/img" + format(img_suffix,"03d") + ".png"
+        render = Image.new('RGBA',SCREEN_SIZE,bgcolor)
+
+        render.paste(static,(0,0),static)
+
+        render.paste(sky_row,(-sky_offset,0),sky_row)
+        if frame_number in minutes:
+            sky_offset -= BLOCK_SIDE
+
+        if offset == 0:
+            scenery = scenery.crop((0,0,SCREEN_WIDTH,SCREEN_HEIGHT))
+            new_scene = Image.new('RGBA',SCREEN_SIZE,(0,0,0,0))
+            for row in [ROWS[1],ROWS[2],ROWS[4],ROWS[5]]:
+                place_scenery(new_scene,row)
+            combined_scene = Image.new('RGBA',(2 * SCREEN_WIDTH,SCREEN_HEIGHT),(0,0,0,0))
+            combined_scene.paste(new_scene,(0,0),new_scene)
+            combined_scene.paste(scenery,(SCREEN_WIDTH,0),scenery)
+            scenery = combined_scene
+            offset = SCREEN_WIDTH
+
+        render.paste(scenery,(-offset,0),scenery)
+        
+        new_filename = "slides/img" + format(frame_number,"04d") + ".png"
+        print("rendering frame " + str(frame_number) + " as " + new_filename)
         render.save(new_filename)
 
-        print("rendering " + new_filename)
+        offset -= BLOCK_SIDE
+        frame_number += 1
 
-        img_suffix += 1
 
-
-def placechoochoo(canvas,x,y):
+def place_engine(canvas,x,y):
     canvas.paste(choochoo,(x,y),choochoo)
 
-def placecar(canvas,x,y):
+def place_car(canvas,x,y):
     if random.choice(["red","green"]) == "red":
         car = redcar
     else:
         car = greencar
     canvas.paste(car,(x,y),car)
 
-def placescenery(canvas,y):
+def place_scenery(canvas,y):
     x = 0    
     for block in range(int(canvas.width)):
         if random.randint(1,10) == 1:
-            canvas.paste(cactus,(x,y),cactus)
+            thing = random.choice([cactus,cactus,palm,palm,horse,turtle])
+            canvas.paste(thing,(x,y),thing)
         x = x + BLOCK_SIDE
 
 if __name__ == "__main__":
